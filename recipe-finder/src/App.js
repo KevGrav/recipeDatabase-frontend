19061e090f7f99d18540122d0c93e1070fdfcbbc
Kev Grav <kgravell.prsvr@gmail.com>
2024-08-01 14:07:02 -0400

    create new folders and styling

--- recipe-finder/src/App.js
+++ recipe-finder/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='Container'>
+    <div className='Header'>
+      <div className='AppName'>
+        Recipe Finder
+      </div>
+      <div className='SearchBox'>
+        <div className='SearchInput'
+          placeholder="Search Recipe"/>
+      </div>
+      <div className='SB2'></div>
+      <div className='SB3'></div>
     </div>
-  );
+    </div>
+    
+  )
 }
 
 export default App;
