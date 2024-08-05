f1ca7b245203488ddc6e9c1ce71f30c624dbd739
Kev Grav <kgravell.prsvr@gmail.com>
2024-08-05 09:24:16 -0400

    updated recipelist.jsx and css, before axios

--- recipe-finder/src/components/RecipeList/RecipeList.jsx
+++ recipe-finder/src/components/RecipeList/RecipeList.jsx
@@ -13,25 +13,56 @@ function RecipeList() {
         Ingredients: "Ingredients",
         SeeCompleteRecipe: "See Complete Recipe"
       }
-
-
   ])
+  const [searchQuery, setSearchQuery] = useState("")
 
-  const [recipeInput, setRecipeInput] = useState("")
+
+
   return (
     <div>
-      <ul>
-      {
-        recipeList.map(recipe =>{  //I don't have a list, I need to fill the recipe containers
-          return(
-            <li key={recipe.id}>
-            {recipe.name}234
-            </li>
-          )
-        } )
-      }
+        <div className='form-div'>
+          <form>
+            <input 
+              type='text'
+              name='searchQuery'
+              placeholder='Search Recipe'
+              onChange={e => setSearchQuery(e.target.value)}
+              value={searchQuery}/>
+            <button type='submit'>Submit</button>
+          </form>
+          <button
+              type='submit'
+              className='favoriteRecipes'
+              onClick={e => getFavorites}
+              >Show Favorites
+          </button>
+          <button
+              type='submit'
+              className='showPantry'
+              onClick={e => getAllPantryItems}
+              >Show Pantry
+          </button> 
+        </div>
 
-      </ul>
+        <div className='recipeList-div'>
+          
+            {/* {recipeList?.length ? (
+              recipeList.map((recipe, index) =>(  
+                <RecipeComponent key={index} recipe={recipe.recipe} />
+              ))
+              ) : (
+              <img className='Placeholder' src="/recipe-finder/food.svg" alt="food pic"/>
+            )} */}
+                {/* return(
+              <li key={recipe.id}>
+              {recipe.name}234
+              </li>
+                )
+              } )
+            } */}
+
+          
+        </div>
     </div>
   )
 }
